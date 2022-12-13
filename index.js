676459a99ff6635fc38b00af228871c2a6d311b1
Shriyash <appollo3579@gmail.com>
2022-12-14 00:28:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+app.use("/assests",express.static("assests"))
+app.set("view engine","ejs");
+require('https').globalAgent.options.rejectUnauthorized = false;
+const photo = require('./Name_inter_IIT.json')
+
+app.get("/",function(req,res){
+    res.render("index",{photo:photo})
+  }
+)
+let port = process.env.PORT;
+if(port==null||port==""){
+  port = 600
+}
+app.listen(port);
